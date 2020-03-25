8a82aba2e6935815e9fe6136b34a33f0eab685f2
David R. Morrison <drmorr@evokewonder.com>
2020-03-24 22:01:21 -0700

    Adding a mini-repl-thing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod components;
+mod repl;
 mod ship;
 mod template;
+
 use std::error::Error;
-use std::io;
 use crate::components::ComponentType;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -12,58 +13,42 @@ fn main() -> Result<(), Box<dyn Error>> {
         template::read_template_file("./data/engines.ron".to_string());
     let mut ship = ship::Ship::new("The Flying Dutchman".to_string(), hull_templates[0].clone());
 
-    let stdin = io::stdin();
     println!(
         "\nGreetings!  You are the captain of {}, a brand new {}!",
         ship.name,
         ship.hull().name
     );
     println!("It currently won't go very far since it's missing an engine.");
-    println!("Would you like to add an engine? (y/n)\n");
-
-    loop {
-        let mut response = String::new();
-        stdin.read_line(&mut response)?;
-        match response.trim() {
-            "y" => break,
-            "n" => {
-                println!("You spend the rest of your life staring wistfully at your ship, wondering what might have been.");
-                return Ok(());
-            },
-            _ => {
-                println!("Sorry, I didn't understand.  Please try again.");
-                continue;
-            },
-        }
+    let choice = repl::get_response("Would you like to add an engine?", vec!["yes", "no"])?;
+    if choice == "no" {
+        println!("You spend the rest of your days wistfully staring at your ship, wondering what might have been.");
+        return Ok(());
     }
 
-    println!("These are the engines currently available; which one do you want?");
+    println!("These are the engines currently available:\n");
     for (i, engine_template) in engine_templates.iter().enumerate() {
         print!("[{}] {}\n", i, engine_template);
     }
+    let choice = repl::get_response(
+        "Which would you like to install?", 
+        (0..engine_templates.len()).collect::<Vec<usize>>(),
+    )?;
 
-    loop {
-        let mut response = String::new();
-        stdin.read_line(&mut response)?;
-        match response.trim().parse::<usize>() {
-            Ok(n) if n < engine_templates.len() => {
-                let res = ship.add_component(ComponentType::Engine(engine_templates[n].clone()), "Fuselage");
-                match res {
-                    Err(s) => println!("Sorry, I couldn't add that engine: {}", s),
-                    _ => break,
-                }
-            },
-            _ => {
-                println!("Invalid choice.  Please try again.");
-                continue;
-            },
+    let mut score = 0;
+    match ship.add_component(ComponentType::Engine(engine_templates[choice].clone()), "Fuselage") {
+        Ok(_) => {
+            println!("You add an engine to your ship and take to the stars.  Game over.\n");
+            score = 1000000000;
+        }
+        Err(_) => {
+            println!("You attempt to force the engine into your ship, but it won't fit.");
+            println!("You try harder, and the engine, your ship, and you, explode.  Game over.\n");
         }
     }
 
-    println!("You add an engine to your ship and take to the stars.  Game over.\n");
     println!("Your ship at the end of the game:\n");
     println!("{}", ship);
-    println!("Final Score: 100000000");
+    println!("Final Score: {}", score);
 
     Ok(())
 }
---
+++ src/repl.rs
@@ -0,0 +1,21 @@
+use std::io::{self, Write};
+
+pub fn get_response<T: ToString + Copy>(prompt: &str, choices: Vec<T>) -> Result<T, io::Error>{
+    loop {
+        let stringify = choices.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" ");
+        print!("{} [{}] ", prompt, stringify);
+        io::stdout().flush()?;
+
+        let mut response = String::new();
+        io::stdin().read_line(&mut response)?;
+        response = response.to_lowercase();
+
+        match choices.iter().position(|c| c.to_string().starts_with(response.trim())) {
+            Some(i) => return Ok(choices[i]),
+            None => {
+                println!("Sorry, I didn't understand.  Please try again.");
+                continue;
+            },
+        }
+    }
+}
